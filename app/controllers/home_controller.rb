96dea0923ffe193bb0f8162096dd6450fce69a03
hajimaru <hajime4515@gmail.com>
2025-01-15 06:29:51 +0900

    中級編 DAY7 Article MVC

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
     def index
         # render 'home/index'　#なくても表示される railsの機能
-        @title = 'デイトラ'
+        # @title = 'デイトラ'
+        @article = Article.first
     end
 
     def about
