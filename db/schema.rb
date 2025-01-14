b0a29b914ae68a09c29fb6de75d687cd332dab6d
hajimaru <hajime4515@gmail.com>
2025-01-15 06:09:53 +0900

    中級編 DAY7 Articleモデル作成(修正)

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2025_01_14_201655) do
 
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
