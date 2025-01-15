a7707c1c5c6a23a8ad64d6370cc9394017affe8a
hajimaru <hajime4515@gmail.com>
2025-01-16 06:14:49 +0900

    中級編 DAY8 Article controller に変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,7 @@
-class HomeController < ApplicationController
+class ArticlesController < ApplicationController
     def index
         # render 'home/index'　#なくても表示される railsの機能
         # @title = 'デイトラ'
         @article = Article.first
-    end
-
-    def about
-        render 'home/about'
     end
 end
