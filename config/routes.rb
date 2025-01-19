0cce3e71af3b11dc7d2cc5016868fe5bf29f63b2
hajimaru <hajime4515@gmail.com>
2025-01-19 13:33:00 +0900

    中級編 DAY15 onlyオプション 不要なものを削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # get '/' => 'home#index'
   root to: 'articles#index' # 必ず使う
 
-  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
