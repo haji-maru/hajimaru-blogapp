c8e5e977c45dba899ec2a024fdc9d0a7b0a813cd
hajimaru <hajime4515@gmail.com>
2025-03-03 05:35:02 +0900

    action text で使用できないものを削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,10 +21,11 @@ class Article < ApplicationRecord
   validates :title, format: { with: /\A(?!\@)/ }
 
   validates :content, presence: true
-  validates :content, length: { minimum: 10 }
-  validates :content, uniqueness: true
+  # action textでは使用できないので削除
+  # validates :content, length: { minimum: 10 }
+  # validates :content, uniqueness: true
 
-  validate :validate_title_and_content_length
+  # validate :validate_title_and_content_length
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -42,9 +43,10 @@ class Article < ApplicationRecord
     likes.count
   end
 
-  private
-  def validate_title_and_content_length
-    char_count = self.title.length + self.content.length
-    errors.add(:content, '100文字以上にしてください') unless char_count > 100
-  end
+  # action textでは使用できないので削除
+  # private
+  # def validate_title_and_content_length
+  #   char_count = self.title.length + self.content.length
+  #   errors.add(:content, '100文字以上にしてください') unless char_count > 100
+  # end
 end
