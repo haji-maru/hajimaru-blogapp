48250bfc317022a38f7c2dddefab130e444e39f9
hajimaru <hajime4515@gmail.com>
2025-03-03 06:13:33 +0900

    followring 設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   has_many :favorite_articles, through: :likes, source: :article
 
   has_many :following_relationshios, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
-
+  has_many :followings, through: :following_relationshios, source: :following
 
   has_one :profile, dependent: :destroy
 
