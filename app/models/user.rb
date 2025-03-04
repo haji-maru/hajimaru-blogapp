2a29e77c600f0a17139d68df79ef0d4594051606
hajimaru <hajime4515@gmail.com>
2025-03-05 05:19:51 +0900

    following 2回定義 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
   # フォロワーの関係
-  has_many :following_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
   has_many :followers, through: :following_relationships, source: :follower
 
   has_one :profile, dependent: :destroy
