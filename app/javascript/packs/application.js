bb1eda186bd2543f63f9c4308a37dd2587db6276
hajimaru <hajime4515@gmail.com>
2025-03-07 06:13:23 +0900

    jquery 起動テスト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,6 +18,10 @@ require("channels")
 require("trix");
 require("@rails/actiontext");
 
+import $ from "jquery";
+
 document.addEventListener("DOMContentLoaded", () => {
-  window.alert("DOM LOADED");
+  $(".article_title").on("click", () => {
+    window.alert("CLICKED");
+  });
 });
