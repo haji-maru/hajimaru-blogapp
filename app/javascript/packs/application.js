e8be65b553e9f742eadcf2bb337c2262d57f3aae
hajimaru <hajime4515@gmail.com>
2025-03-08 06:15:46 +0900

    hiddenクラス 付外し 追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,6 +28,11 @@ document.addEventListener("turbolinks:load", () => {
   const dataset = $("#article-show").data();
   const articleId = dataset.articleId;
   axios.get(`/articles/${articleId}/like`).then((response) => {
-    console.log(response);
+    const hasLiked = response.data.hasLiked;
+    if (hasLiked) {
+      $(".active-heart").removeClass("hidden");
+    } else {
+      $(".inactive-heart").removeClass("hidden");
+    }
   });
 });
