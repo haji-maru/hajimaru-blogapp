9056422f05086549037df18e6f1d11bb04f92958
hajimaru <hajime4515@gmail.com>
2025-03-08 12:28:56 +0900

    comment index path 追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,10 @@ class CommentsController < ApplicationController
     @comment = article.comments.build
   end
 
+  def index
+    
+  end
+
   def create
     article = Article.find(params[:article_id])
     @comment = article.comments.build(comment_params)
