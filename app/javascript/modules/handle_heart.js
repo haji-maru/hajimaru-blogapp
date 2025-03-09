761c523e6ace5dce878a8259de9e70e6e2ad0b91
hajimaru <hajime4515@gmail.com>
2025-03-09 13:11:59 +0900

    jquery axios rails-ujs 読込

--- app/javascript/modules/handle_heart.js
+++ app/javascript/modules/handle_heart.js
@@ -1,3 +1,9 @@
+import $ from "jquery";
+import axios from "axios";
+import { csrfToken } from "rails-ujs";
+
+axios.defaults.headers.common["X-CSRF-Token"] = csrfToken();
+
 // ハート赤になる
 const listenInactiveHeartEvent = (articleId) => {
   $(".inactive-heart").on("click", () => {
