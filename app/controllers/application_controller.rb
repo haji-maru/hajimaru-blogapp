3c60b652c9480361a53d961f8e3ec3411ff5ba9c
hajimaru <hajime4515@gmail.com>
2025-03-10 06:05:07 +0900

    初期の言語設定 default は日本語

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   def current_user
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
     super
+  end
+
+  private
+  def set_locale
+    # 設定言語を変更する
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
