35f7bddc2cfad7432a7870ae2fe518ca9afcf817
hajimaru <hajime4515@gmail.com>
2025-03-09 11:53:12 +0900

    リロードなしでコメント追加

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -37,7 +37,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // コメント追加
   $(".add-comment-button").on("click", () => {
-    const content = $("comment_content").val();
+    const content = $("#comment_content").val();
+    if (!content) {
+      window.alert("コメントを入力してください");
+    } else {
+      axios
+        .post(`/articles/${articleId}/comments`, {
+          comment: { content: content },
+        })
+        .then((response) => {
+          const comment = response.data;
+          $(".comments-container").append(
+            `<div class="article_comment"><p>${comment.content}</p></div>`
+          );
+        });
+      $("#comment_content").val("");
+    }
   });
 
   // いいねボタン切替
