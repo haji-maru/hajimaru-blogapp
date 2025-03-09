35be132c97214e8fee202b39ecaf8f1bae4e21f1
hajimaru <hajime4515@gmail.com>
2025-03-09 16:00:30 +0900

    perform 全員にメール送信 非同期処理

--- app/jobs/notification_from_admin_job.rb
+++ app/jobs/notification_from_admin_job.rb
@@ -2,6 +2,9 @@ class NotificationFromAdminJob < ApplicationJob
   queue_us :default
 
   # perform 必須
-  def perform
+  def perform(msg)
+    User.all.each do |user|
+      NotificationFromAdminMailer.notify(user, msg).deliver_later
+    end
   end
 end
