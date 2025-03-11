13bb914b9427c708a61399ee58bbab516700f76a
hajimaru <hajime4515@gmail.com>
2025-03-12 05:30:36 +0900

    ユーザーと記事のダミーデータ作成

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   it 'タイトルと内容が入力されていれば、記事を保存できる' do
-
+    user = User.create!({
+      email: 'test@example.com',
+      password: 'password'
+    })
+    article = user.articles.build({
+      title: Faker::lorem.characters(number: 10),
+      content: Faker::lorem.characters(number: 300)
+    })
   end
 end
