8d585bb385b2cfa81acb2eb3f9b2e201223c9671
hajimaru <hajime4515@gmail.com>
2025-03-12 06:25:31 +0900

    タイトルと内容が入力されている場合  修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,12 +4,7 @@ RSpec.describe Article, type: :model do
   let!(:user) { create(:user) }
 
   context 'タイトルと内容が入力されている場合' do
-    let!(:article) do
-      user.articles.build({
-        title: Faker::Lorem.characters(number: 10),
-        content: Faker::Lorem.characters(number: 300)
-      })
-    end
+    let!(:article) { build(:article, user: user) }
 
     it '記事を保存できる' do
       expect(article).to be_valid
