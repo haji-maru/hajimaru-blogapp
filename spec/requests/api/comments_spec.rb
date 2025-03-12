68f16c45f4dcb1501d6a7adc11980106e93203cd
hajimaru <hajime4515@gmail.com>
2025-03-13 06:04:34 +0900

    article  に紐づく comments  作成

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Api::Comments', type: :request do
   describe 'GET /api/comments' do
     let!(:user) { create(:user) }
     let!(:article) { create(:article, user: user) }
+    let!(:comments) { create_list(:comment, 3, article: article) }
 
     it '200ステータス' do
       get api_comments_path
