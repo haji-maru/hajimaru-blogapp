9ac680644859487a501bbfea214d1e31c7f227ec
hajimaru <hajime4515@gmail.com>
2025-03-13 05:59:30 +0900

    タイトル変更

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Api::Comments', type: :request do
   describe 'GET /api/comments' do
-    it 'works! (now write some real specs)' do
+    it '200ステータス' do
       get api_comments_path
       expect(response).to have_http_status(200)
     end
